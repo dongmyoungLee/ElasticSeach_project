1bf0cec25271552c1510de2b2e2d5efbe3588322
gotoAbraxas <lty7085@gmail.com>
2023-06-27 16:47:27 +0900

    testFile

---
+++ src/main/java/com/naver/config/ElasticsearchConfig.java
@@ -0,0 +1,18 @@
+package com.naver.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+
+    public static RestHighLevelClient elasticsearchClient() {
+        return new RestHighLevelClient(
+                RestClient.builder(HttpHost.create("localhost:9200"))
+        );
+    }
+}
